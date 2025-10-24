352c495b0ddb3c1b3d14c28e96adc66307d653b0
MARKETTV1 <aliridakarim8@gmail.com>
2025-10-24 03:14:21 +0100

    Refactor testinstall.sh to include shebang and message
    Added a shebang and success message to the script.

--- testinstall.sh
+++ testinstall.sh
@@ -1,4 +1,6 @@
-curl -L -o /tmp/Dream_Stream.tar "https://github.com/Said-Pro/StalkerOS/raw/refs/heads/main/Dream_Stream.tar"
-tar -xf /tmp/Dream_Stream.tar -C /tmp/
-cp -r /tmp/Dream_Stream /usr/lib/enigma2/python/Plugins/Extensions/
-systemctl restart enigma2
+#!/bin/bash
+curl -L -o /tmp/Dream_Stream.tar "https://github.com/Said-Pro/StalkerOS/raw/refs/heads/main/Dream_Stream.tar" && \
+tar -xf /tmp/Dream_Stream.tar -C /tmp/ && \
+cp -r /tmp/Dream_Stream /usr/lib/enigma2/python/Plugins/Extensions/ && \
+systemctl restart enigma2 && \
+echo "تم التثبيت بنجاح!"
